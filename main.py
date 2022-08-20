fc03181371ac058d3841e0d10aa934ba020b16b8
boldueen <83167994+boldueen@users.noreply.github.com>
2022-08-20 17:04:50 +0300

    fix server bugs

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+from selenium.webdriver import FirefoxOptions
+
 import time
 
 start = time.time()
 print('_____start____programm____')
 
+opts = FirefoxOptions()
+opts.add_argument("--headless")
 
-
-driver = webdriver.Firefox(executable_path = './utils/geckodriver')
+driver = webdriver.Firefox(executable_path = './utils/geckodriver', options=opts)
 
 driver.get("http://www.python.org")
 
